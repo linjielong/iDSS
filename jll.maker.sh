52c6da319412c5def69d0d86cc6c63b66bf65446
jielong.lin <jielong.lin@qq.com>
2017-06-21 14:57:54 +0800

    update by jll.Git.sh @ 2017-06-21 14:57:54 Solve the compiled issue about undefined reference to 'pow', 'clock_nanosleep' and so on. Remove the old patch for solving the above same compiled issue. Add the automatical building production tools 'jll.maker.sh' to support for perl building

--- jll.maker.sh
+++ jll.maker.sh
@@ -5,7 +5,7 @@
 #   Author:       root
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-21 00:09:54
-#   ModifiedTime: 2017-06-21 01:14:52
+#   ModifiedTime: 2017-06-21 11:27:20
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
@@ -13,9 +13,69 @@ source ${JLLPATH}/.BashShellLibrary
 
 declare -i GvPageUnit=10
 declare -a GvPageMenuUtilsContent=(
-  "build_pipeline: compile perl -- compile dss -- install dss"
-  "clean_pipeline: uninstall dss -- clean dss -- clean perl"
+  "build_pipeline: ...perl...dss..."
+  "clean_pipeline: ...dss...perl..."
 )
+
+function _FN_exit()
+{
+    [ x"${GvPageUnit}" != x ] && unset GvPageUnit
+    [ x"${GvPageMenuUtilsContent}" != x ] && unset GvPageMenuUtilsContent
+    echo
+    exit 0
+}
+
+_DT_=$(date +%Y_%m_%d__%H_%M_%S)
+_JLLCFG_SRC_PERL="$(pwd)/perl-5.8.9"
+_JLLCFG_BIN_PERL="/usr/local/DarwinStreamingServer/perl-5.8.9/bin/perl"
+function _FN_build_perl()
+{
+more >&1<<EOF
+
+${Bseablue}${Fblack}==================================================${AC}
+${Bseablue}${Fblack} ...${Fred}perl${Fblack}...dss...                                 ${AC}
+${Bseablue}${Fblack}==================================================${AC}
+
+EOF
+    # Check if perl is installed or not
+    if [ ! -e "${_JLLCFG_BIN_PERL}" ]; then
+        if [ ! -e "${_JLLCFG_SRC_PERL}" ]; then
+            echo -e "${Bred}${Fyellow}JLL-Exit:${AC} not found" \
+                    "${Bred}${Fwhite}${_JLLCFG_SRC_PERL}${AC}"
+            _FN_exit 
+        fi
+        cd ${_JLLCFG_SRC_PERL}
+        [ x"$(ls | grep -Ei 'makefile ')" != x ] && make clean
+        [ x"$(ls | grep -Ei 'make_perl_at_.*\.log')" != x ] && rm -rf make_perl_at_*.log
+        ./Configure -des -Dprefix=${_JLLCFG_BIN_PERL} 2>&1 | tee make_perl_at_${_DT_}.log
+        if [ x"$(ls | grep -Ei '^makefile$')" != x ]; then
+            make 2>&1 | tee -a make_perl_at_${_DT_}.log
+            make install 2>&1 | tee -a make_perl_at_${_DT_}.log
+        else
+             echo -e "${Bred}${Fyellow}JLL-Exit:${AC} ${Fred}perl${AC} is configured failure"
+             cd - >/dev/null
+            _FN_exit 
+        fi 
+        cd - >/dev/null
+        if [ ! -e "${_JLLCFG_BIN_PERL}" ]; then
+            echo -e "${Bred}${Fyellow}JLL-Exit:${AC} ${Fred}perl${AC} compile or install failure"
+            _FN_exit
+        else
+            echo -e "JLL: ${Fred}perl${AC} was installed to" \
+                    "${Bgreen}${Fblack}${_JLLCFG_BIN_PERL}${AC}"
+        fi
+    else
+        echo -e "JLL: ${Fred}perl${AC} has already been installed to" \
+                 "${Bgreen}${Fblack}${_JLLCFG_BIN_PERL}${AC}"
+    fi
+}
+
+
+function _FN_build_pipeline()
+{
+    _FN_build_perl
+}
+
 echo
 Lfn_PageMenuUtils _MenuID  "Select" 7 4 \
                             "***** Menu For Darwin Streaming Server  (q: quit) *****"
@@ -23,10 +83,7 @@ if [ x"${_MenuID}" = x"${GvPageMenuUtilsContent[0]}" ]; then
     [ x"${_MenuID}" != x ] && unset _MenuID
     [ x"${GvPageMenuUtilsContent}" != x ] && unset GvPageMenuUtilsContent
     [ x"${GvPageUnit}" != x ] && unset GvPageUnit
-more >&1<<EOF
-${Bseablue}${Fblack}compile perl${AC}
-EOF
-    
+    _FN_build_pipeline
     exit 0
 fi
 if [ x"${_MenuID}" = x"${GvPageMenuUtilsContent[1]}" ]; then
